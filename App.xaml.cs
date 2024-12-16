edad5306a914cb2ba935cd635b45b8c41c0bea08
Ronisha Shrestha <shrestharonisha374@gmail.com>
2024-12-16 11:06:23 +0545

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackMyExpenses
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
