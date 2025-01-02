c947e5ed9d0e7aa029683ead0f75503e52ea5e32
Asim Khadka <asimkhadka52@gmail.com>
2025-01-02 08:59:56 +0545

    BudgetEase Project setup

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BudgetEase
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
